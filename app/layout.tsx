f1870496b0f159e9ca555f9f4455c0494c9de51c
egidem65-boop <egidem65-boop@users.noreply.github.com>
2026-04-18 14:47:20 +0200

    header again

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
     >
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
-        <link rel="stylesheet" href="/style.css" />
       </head>
       <body className="min-h-full flex flex-col">
         <ConfirmProvider>
